14a39c32143b4bf913ec99e6dad66d5afd825414
OksanaBakl <oksanamolesha@ukr.net>
2021-07-19 12:59:29 +0300

    change url

--- src/index.js
+++ src/index.js
@@ -10,17 +10,10 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 const bodyRef = document.querySelector('body');
 const switchEl = document.querySelector('#theme-switch-toggle');
 switchEl.addEventListener('change', onChangedSwitch);
-
-if (localStorage.getItem('theme') === Theme.DARK) {
-  bodyRef.classList.add(Theme.DARK);
-  switchEl.checked = 'true';
-} else {
-  localStorage.setItem('theme', Theme.LIGHT);
-  bodyRef.classList.add(Theme.LIGHT);
-}
 
 function onChangedSwitch() {
   if (localStorage.theme === Theme.LIGHT) {
@@ -30,4 +23,12 @@ function onChangedSwitch() {
     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
   }
+}
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
+  switchEl.checked = 'true';
+} else {
+  localStorage.setItem('theme', Theme.LIGHT);
+  bodyRef.classList.add(Theme.LIGHT);
 }
