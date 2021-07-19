92cbe2b335b43146018dd71ccb3d1cbe9eedf8c2
OksanaBakl <oksanamolesha@ukr.net>
2021-07-19 13:10:09 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,18 +2,36 @@ import './styles.css';
 import menu from './menu.json';
 import menuCardsTpl from './templates/menu.hbs';
 
+//================= menuMarkup on templates =================
+
 const menuRef = document.querySelector('.js-menu');
 const menuMarkup = menuCardsTpl(menu);
 menuRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+//======================= Theme ==============================
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+//================= listener on change Switch =================
+
 const bodyRef = document.querySelector('body');
 const switchEl = document.querySelector('#theme-switch-toggle');
 switchEl.addEventListener('change', onChangedSwitch);
+
+//================= apply current Theme ==========================
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
+  switchEl.checked = 'true';
+} else {
+  localStorage.setItem('theme', Theme.LIGHT);
+  bodyRef.classList.add(Theme.LIGHT);
+}
+
+//================= function on change Switch =================
 
 function onChangedSwitch() {
   if (localStorage.theme === Theme.LIGHT) {
@@ -23,12 +41,4 @@ function onChangedSwitch() {
     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
   }
-}
-
-if (localStorage.getItem('theme') === Theme.DARK) {
-  bodyRef.classList.add(Theme.DARK);
-  switchEl.checked = 'true';
-} else {
-  localStorage.setItem('theme', Theme.LIGHT);
-  bodyRef.classList.add(Theme.LIGHT);
 }
